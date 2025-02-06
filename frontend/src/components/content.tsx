9ad34c874ad8c22b77b0b2b2b50ee5c2e0225c41
Tobias Messner <tobias@zusor.io>
2025-02-06 11:51:03 +0100

    fix: Alway scroll to top for Markdown content
    Closes #56

--- frontend/src/components/content.tsx
+++ frontend/src/components/content.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useEffect, useRef } from "react";
 import { $api } from "@/lib/api/client.ts";
 import { RenderMdx } from "@/components/render-mdx.tsx";
 import { ScrollArea } from "./ui/scroll-area";
@@ -22,9 +23,20 @@ const Content = ({ path }: { path: string }) => {
     },
   );
 
+  const scrollAreaRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollAreaRef.current) {
+      scrollAreaRef.current.scrollTop = 0;
+    }
+  }, [path]);
+
   return (
     <>
-      <ScrollArea className="prose mx-auto max-w-screen-md grow rounded-md border-2 p-4 prose-h1:text-3xl prose-h1:font-bold prose-img:max-h-72 prose-img:max-w-full prose-img:object-contain">
+      <ScrollArea
+        viewportRef={scrollAreaRef}
+        className="prose mx-auto max-w-screen-md grow rounded-md border-2 p-4 prose-h1:text-3xl prose-h1:font-bold prose-img:max-h-72 prose-img:max-w-full prose-img:object-contain"
+      >
         <RenderMdx source={data.content} path={path} />
         {data.has_quiz && <Quiz path={path} />}
         {data.exercise && <Exercise path={path} />}
