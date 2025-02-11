137af07fd8451c71533a82dc9b2b4e8131213e43
Tobias Messner <tobias@zusor.io>
2025-02-11 16:53:49 +0100

    feat: Add Project auto-loading
    Closes #17

--- frontend/src/components/project-control.tsx
+++ frontend/src/components/project-control.tsx
@@ -5,6 +5,7 @@
 
 import { $api } from "@/lib/api/client.ts";
 import { Button } from "@/components/ui/button.tsx";
+import { useEffect, useState } from "react";
 
 const ProjectControl = ({ path }: { path: string }) => {
   const { data: lessonData } = $api.useSuspenseQuery(
@@ -38,6 +39,28 @@ const ProjectControl = ({ path }: { path: string }) => {
     "post",
     "/training/lesson/{lesson_path}/load_project",
   );
+
+  const [hasAutoloadedProject, setHasAutoloadedProject] = useState(false);
+
+  useEffect(() => {
+    if (!lessonData.start_project || lessonData.show_capella === false) return;
+    if (
+      (projectStatus === "WRONG_PROJECT" || projectStatus === "UNLOADED") &&
+      !hasAutoloadedProject
+    ) {
+      setHasAutoloadedProject(true);
+      loadProject.mutate({
+        params: { path: { lesson_path: path } },
+      });
+    }
+  }, [
+    hasAutoloadedProject,
+    lessonData.show_capella,
+    lessonData.start_project,
+    loadProject,
+    path,
+    projectStatus,
+  ]);
 
   if (!lessonData.start_project || lessonData.show_capella === false) {
     return null;
